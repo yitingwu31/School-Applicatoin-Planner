66b37ced0ffaf51d712ecce679778ee38a853fdd
SUNGHSINTZU <gloria30721@gmail.com>
2021-06-26 12:26:49 +0800

    add school

--- frontend/src/Components/NavBar.js
+++ frontend/src/Components/NavBar.js
@@ -1,8 +1,10 @@
-import { useState } from 'react' 
+import { useState, useEffect } from 'react'
 import { Toolbar, IconButton, Button, Menu, MenuItem } from '@material-ui/core'
 import AddCircleIcon from '@material-ui/icons/AddCircle'
 import { Link as MaterialLink } from '@material-ui/core'
 import { Link as RouterLink } from 'react-router-dom'
+import { OpenInBrowserRounded } from '@material-ui/icons'
+import AddSchool from './AddSchool'
 
 const NavBar = () => {
     const [anchorEl, setAnchorEl] = useState(null)
@@ -10,24 +12,38 @@ const NavBar = () => {
     const handleClick = (event) => {
         setAnchorEl(event.currentTarget)
     }
-    
+
     const handleClose = () => {
         setAnchorEl(null)
     }
+    // Add School
+    const [addOpen, setAddOpen] = useState(false);
+    const [addSchool, setAddSchool] = useState({})
+    const handleAddOpen = () => {
+        setAddOpen(true);
+    };
+    const handleAddClose = () => {
+        setAddOpen(false);
+    };
+
+    useEffect(() => {
+        console.log(addSchool)
+    }, [addSchool])
 
     return (
         <div>
             <Toolbar>
-                <Button aria-controls="view-menu"  aria-haspopup="true" onClick={handleClick}>
+                <Button aria-controls="view-menu" aria-haspopup="true" onClick={handleClick}>
                     View
                 </Button>
                 <Menu id="view-menu" anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose}>
                     <MenuItem onClick={handleClose} component={RouterLink} to="/schools">School</MenuItem>
                     <MenuItem onClick={handleClose} component={RouterLink} to="/calendar">Calendar</MenuItem>
                 </Menu>
-                <IconButton aria-label="add school" color="primary" component={RouterLink} to="/addSchool">
+                <IconButton aria-label="add school" color="primary" onClick={handleAddOpen}>
                     <AddCircleIcon />
                 </IconButton>
+                <AddSchool handleClose={handleAddClose} open={addOpen} setAddSchool={setAddSchool} />
             </Toolbar>
         </div>
     )
