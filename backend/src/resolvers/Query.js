b64c020d406bd330b3b5e2bf1a006a728dbe4fa8
yitingwu31 <animalfamily31@gmail.com>
2021-06-29 16:33:03 +0800

    feat: query all by date (w/out school)

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,12 +1,39 @@
-import { makeSchoolKey } from "../utils";
+import { makeSchoolKey, checkDeadline } from "../utils";
+
+const findUser = async (name, db) => {
+    const user = await db.UserModel.findOne({ name });
+    return user
+        .populate('schools')
+        .populate({ path: 'schools', populate: 'todos' })
+        .populate({ path: 'schools', populate: { path: 'todos', populate: 'checkpoints' }})
+        .execPopulate();
+}
+
+const findTodo = async (user, year, month, db) => {
+    let allTodos = await db.TodoModel.find().populate('checkpoints');
+    let findtodos = allTodos.filter((todo) => {
+        return todo.key.split('-')[0] === user;
+    })
+    findtodos = findtodos.filter((todo) => {
+        return checkDeadline(year, month, todo.deadline);
+    })
+    return findtodos;
+}
+
+const findCheckpoint = async (user, year, month, db) => {
+    let allCheckpoints = await db.CheckpointModel.find();
+    let findcheckpoints = allCheckpoints.filter((check) => {
+        return check.key.split('-')[0] === user;
+    })
+    findcheckpoints = findcheckpoints.filter((check) => {
+        return checkDeadline(year, month, check.time);
+    })
+    return findcheckpoints;
+}
 
 const Query = {
     async user(parent, { name }, { db }, info) {
-        const user = await db.UserModel.findOne({ name });
-        return user
-            .populate('schools')
-            .populate({ path: 'schools', populate: 'todos' })
-            .execPopulate();
+        return findUser(name, db);
     },
 
     async userSchool(parent, { user, school }, { db }, info) {
@@ -19,27 +46,38 @@ const Query = {
     },
 
     async userMonthTodo(parent, { user, year, month }, { db }, info) {
-        let allTodos = await db.TodoModel.find().populate('checkpoints');
-        let findtodos = allTodos.filter((todo) => {
-            return todo.key.split('-')[0] === user;
-        })
-        findtodos = findtodos.filter((todo) => {
-            let date = todo.deadline.split('-');
-            return date[0] == year && date[1] == month;
-        })
-        return findtodos;
+        return findTodo(user, year, month, db);
     }, 
 
     async userMonthCheckpoint(parent, { user, year, month }, { db }, info) {
-        let allCheckpoints = await db.CheckpointModel.find();
-        let findcheckpoints = allCheckpoints.filter((check) => {
-            return check.key.split('-')[0] === user;
+        return findCheckpoint(user, year, month, db);
+    },
+
+    async allByDate(parent, { user, year, month }, { db }, info) {
+        let ret = [];
+        // let schools = await findUser(user, db).schools;
+        // console.log(schools)
+        // schools.forEach((school) => {
+        //     if (checkDeadline(year, month, school.deadline)) {
+        //         ret.push({ type: 'school', context: school.name, deadline: school.deadline});
+        //     }
+        // })
+        let todos = await findTodo(user, year, month, db);
+        let checkpoints = await findCheckpoint(user, year, month, db);
+        todos.map((todo) => {
+            if (checkDeadline(year, month, todo.deadline)) {
+                let s = todo.key.split('-');
+                ret.push({ type: 'todo', context: `${s[1]} ${todo.task}`, deadline: todo.deadline});
+            }
         })
-        findcheckpoints = findcheckpoints.filter((check) => {
-            let date = check.time.split('-');
-            return date[0] == year && date[1] == month;
+        checkpoints.map((check) => {
+            if (checkDeadline(year, month, check.time)) {
+                let s = check.key.split('-');
+                ret.push({ type: 'checkpoint', context: `${s[1]} ${s[2]} ${check.content}`, deadline: check.time});
+            }
         })
-        return findcheckpoints;
+        console.log(ret);
+        return ret;
     }
 };
 
